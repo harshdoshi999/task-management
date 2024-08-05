df7f2cc3d6575b4ee754b85b8efb2a20b9998628
Harsh Doshi <harshdoshi999@gmail.com>
2024-08-05 17:58:18 +0530

    initial commit with basic setup

---
+++ web/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+        <h1 className="text-3xl font-bold underline">
+          Hello world!!!
+        </h1>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ web/src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer class="font-sans tracking-wide bg-[#213343] py-10 px-10">
+            <div class="text-center">
+                <p class="text-gray-300 text-[15px]">
+                    Task Management Application
+                </p>
+            </div>
+        </footer>
+    )
+}
